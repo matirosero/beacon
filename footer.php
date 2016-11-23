03371e4de337ec3b36bab8893e9438b39d52e586
MatiRosero <gekidasa@gmail.com>
2016-11-23 17:02:21 -0600

    footer info

--- footer.php
+++ footer.php
@@ -16,11 +16,16 @@
 
 				<div class="column row">
 
+					<p class="footer-logo text-center">
+		              <a href="<?php esc_attr_e( home_url( '/' ) ); ?>" rel="home">
+		                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/svg/logo-beacon-footer.svg" alt="<?php bloginfo( 'name' ); ?>" />
+		              </a>
+		            </p>
+
 					<p class="text-center">
-						<svg class="icon">
-							<use xlink:href="#icon-coffee-cup"></use>
-						</svg>
-						Thanks for using Beacon!
+						&copy; <?php echo date("Y"); ?> <?php echo get_bloginfo( 'name' ); ?>, todos los derechos reservados
+						<br />
+						Desarrollo web: <a href="http://matilderosero.com">Matilde Rosero</a> | Mercadeo: <a href="mailto:michelle.morera@gmail.com">Michelle Morera</a>
 					</p>
 
 				</div><!-- .column.row -->
