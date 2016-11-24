c1b11917780ae80974eef42006c67b71e74f7672
MatiRosero <gekidasa@gmail.com>
2016-11-24 00:31:42 -0600

    profile excerpt in frontpage

--- front-page.php
+++ front-page.php
@@ -21,9 +21,9 @@ get_header(); ?>
 
 			<?php
 
-			if ( have_posts() ) :
+			// if ( have_posts() ) :
 
-				while ( have_posts() ) : the_post(); ?>
+			// 	while ( have_posts() ) : the_post(); ?>
 
 					<section id="home-how-help">
 						<h2 class="section-title">Como le podemos ayudar</h2>
@@ -41,21 +41,69 @@ get_header(); ?>
 
 					<section id="home-services">
 						<h2 class="section-title">Servicios</h2>
-						<?php the_content(); ?>
+						<?php //the_content(); ?>
+						<?php
+						$services  = get_post_meta( get_the_ID(), 'frontpage_services', true );
+						echo $services;
+						?>
 					</section>
 
 					<section id="home-profiles">
-						Perfiles
+						<?php
+							/**
+							 * The WordPress Query class.
+							 * @link http://codex.wordpress.org/Function_Reference/WP_Query
+							 *
+							 */
+							$args = array(
+
+								//Type & Status Parameters
+								'post_type'   => 'profile',
+								// 'post_status' => 'publish',
+
+								//Order & Orderby Parameters
+								'order'               => 'ASC',
+								// 'orderby'             => 'date',
+
+								//Pagination Parameters
+								'posts_per_page'         => -1,
+
+								//Parameters relating to caching
+								'no_found_rows'          => false,
+								'cache_results'          => true,
+								'update_post_term_cache' => true,
+								'update_post_meta_cache' => true,
+
+							);
+
+						$query = new WP_Query( $args );
+
+
+						$count = $query->post_count;
+
+						if ( $count == 2 ) :
+							while ( $query->have_posts() ) : $query->the_post();
+
+								$title = get_the_title(); // This must be!, because this is the return - the_title would be echo
+								$title_array = explode(' ', $title);
+								$firstname = $title_array[0];
+
+								echo '¡Hola! Yo soy '.$firstname;
+								the_excerpt();
+
+							endwhile;
+						endif;
+						?>
 					</section>
 
 				<?php
-				endwhile;
+				// endwhile;
 
-			else :
+			// else :
 
-				get_template_part( 'template-parts/content', 'none' );
+			// 	get_template_part( 'template-parts/content', 'none' );
 
-			endif; ?>
+			// endif; ?>
 
 		</main>
 
