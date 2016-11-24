2f0870db302443439b9b5ada03d8b7fd49729b91
MatiRosero <gekidasa@gmail.com>
2016-11-24 00:08:18 -0600

    cpt

---
+++ page-templates/coaching.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Coaching
+ * The template for displaying information about coaching.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Beacon
+ */
+
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/content', 'hero' ); ?>
+
+<div class="row">
+	<div id="primary" class="content-area medium-9 small-centered columns">
+
+		<main id="main" class="site-main" role="main">
+		
+			<section id="coaching-steps">
+				Steps
+			</section>
+
+			<section id="appointment-form">
+				Appointment request form
+			</section>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .row -->
+
+<?php get_footer(); ?>
