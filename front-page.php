8cef8815c3fd2cc165923c85eca5d91c8cf70e0d
MatiRosero <gekidasa@gmail.com>
2016-11-24 10:05:48 -0600

    frontpage layout skeleton

--- front-page.php
+++ front-page.php
@@ -9,107 +9,145 @@
 
 get_header(); ?>
 
-<div class="row column expanded small-collapse">
+<header id="home-header">
+	Home header
+</header>
 
-	<header id="home-header">
-		Home header
-	</header>
+<div id="primary" class="content-area">
 
-	<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-		<main id="main" class="site-main" role="main">
+		<?php
+		// if ( have_posts() ) :
+		// 	while ( have_posts() ) : the_post(); ?>
 
-			<?php
+		<section id="home-how-help" class="page-section">
+			<div class="row">
+				<div class="medium-4 columns">
+					<h2 class="section-title">Como le podemos ayudar</h2>
+				</div>
+				<div class="medium-8 columns">
+					<?php
+					$intro  = get_post_meta( get_the_ID(), 'frontpage_intro', true );
+					echo $intro;
+					?>
+				</div>
+			</div>
+		</section>
 
-			// if ( have_posts() ) :
+		<?php /*
+		<section id="home-test">
+			test
+		</section>
+		*/ ?>
 
-			// 	while ( have_posts() ) : the_post(); ?>
+		<section id="home-services" class="page-section">
+			<div class="row">
+				<div class="medium-4 columns">
+					<h2 class="section-title">Servicios</h2>
+				</div>
+				<div class="medium-8 columns">
+					<?php //the_content(); ?>
+					<?php
+					$services  = get_post_meta( get_the_ID(), 'frontpage_services', true );
+					echo $services;
+					?>
+				</div>
+			</div>
+		</section>
 
-					<section id="home-how-help">
-						<h2 class="section-title">Como le podemos ayudar</h2>
-						<?php
-						$intro  = get_post_meta( get_the_ID(), 'frontpage_intro', true );
-						echo $intro;
-						?>
-					</section>
+		<?php
+			/**
+			 * The WordPress Query class.
+			 * @link http://codex.wordpress.org/Function_Reference/WP_Query
+			 *
+			 */
+			$args = array(
 
-					<?php /*
-					<section id="home-test">
-						test
-					</section>
-					*/ ?>
+				//Type & Status Parameters
+				'post_type'   => 'profile',
+				// 'post_status' => 'publish',
 
-					<section id="home-services">
-						<h2 class="section-title">Servicios</h2>
-						<?php //the_content(); ?>
-						<?php
-						$services  = get_post_meta( get_the_ID(), 'frontpage_services', true );
-						echo $services;
-						?>
-					</section>
+				//Order & Orderby Parameters
+				'order'               => 'DESC',
+				// 'orderby'             => 'date',
 
-					<section id="home-profiles">
-						<?php
-							/**
-							 * The WordPress Query class.
-							 * @link http://codex.wordpress.org/Function_Reference/WP_Query
-							 *
-							 */
-							$args = array(
+				//Pagination Parameters
+				'posts_per_page'         => -1,
 
-								//Type & Status Parameters
-								'post_type'   => 'profile',
-								// 'post_status' => 'publish',
+				//Parameters relating to caching
+				'no_found_rows'          => false,
+				'cache_results'          => true,
+				'update_post_term_cache' => true,
+				'update_post_meta_cache' => true,
 
-								//Order & Orderby Parameters
-								'order'               => 'ASC',
-								// 'orderby'             => 'date',
+			);
 
-								//Pagination Parameters
-								'posts_per_page'         => -1,
+		$query = new WP_Query( $args );
 
-								//Parameters relating to caching
-								'no_found_rows'          => false,
-								'cache_results'          => true,
-								'update_post_term_cache' => true,
-								'update_post_meta_cache' => true,
+		$count = $query->post_count;
 
-							);
+		if ( $count == 2 ) : ?>
 
-						$query = new WP_Query( $args );
-
-						$count = $query->post_count;
-
-						if ( $count == 2 ) :
-							while ( $query->have_posts() ) : $query->the_post();
-
-								$title = get_the_title(); // This must be!, because this is the return - the_title would be echo
-								$title_array = explode(' ', $title);
-								$firstname = $title_array[0];
-
-								echo '¡Hola! Yo soy '.$firstname;
-								the_excerpt();
-
-							endwhile;
-						endif;
-						?>
-					</section>
+			<section id="home-profiles" class="page-section row column expanded">
 
 				<?php
-				// endwhile;
 
-			// else :
+				while ( $query->have_posts() ) : $query->the_post();
 
-			// 	get_template_part( 'template-parts/content', 'none' );
+					// Count.
+					$i = ! isset( $i ) ? 1 : $i;
 
-			// endif; ?>
+					// Does this post count odd?
+					$odd = $i % 2; // 1/0
 
-		</main>
+					$title = get_the_title(); // This must be!, because this is the return - the_title would be echo
+					$title_array = explode(' ', $title);
+					$firstname = $title_array[0];
 
-	</div>
+					?>
 
+					<div class="medium-6 columns">
+
+						<?php
+
+						if ($odd) :
+							echo 'odd';
+						else:
+							echo 'even';
+						endif;
+
+
+						echo '¡Hola! Yo soy '.$firstname;
+						the_excerpt();
+						?>
+
+					</div>
+
+					<?php
+
+					// Count up.
+					$i++;
+
+				endwhile; ?>
+
+			</section>
+
+		<?php endif; ?>
+
+
+			<?php
+			// endwhile;
+		// else :
+		// 	get_template_part( 'template-parts/content', 'none' );
+		// endif; ?>
+
+	</main>
 
 </div>
+
+
+
 
 <?php
 get_footer();
