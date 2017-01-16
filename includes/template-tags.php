8dbbf3a2b4dd692fb5d50cf23f181f3ee3d428cb
MatiRosero <gekidasa@gmail.com>
2017-01-16 11:48:55 -0600

    link to gerardo profile from blog posts

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -28,8 +28,12 @@ function beacon_posted_on() {
 
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'beacon' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_page_link(12) ) . '#gerardo-lizano">' . esc_html( get_the_author() ) . '</a></span>'
 	);
+	// $byline = sprintf(
+	// 	esc_html_x( 'by %s', 'post author', 'beacon' ),
+	// 	'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	// );
 
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 }
