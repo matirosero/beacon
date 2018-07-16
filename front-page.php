ac5efb04002719f40fe8fdf71eec2eb812db39a3
MatiRosero <gekidasa@gmail.com>
2018-07-15 19:53:00 -0600

    remove modal thingu

--- front-page.php
+++ front-page.php
@@ -66,28 +66,7 @@ get_header(); ?>
 
 		<?php get_template_part( 'template-parts/frontpage', 'profiles' ); ?>
 
-		<p><a data-open="exampleModal1">Click me for a modal</a></p>
-
-
-			<?php
-			// endwhile;
-		// else :
-		// 	get_template_part( 'template-parts/content', 'none' );
-		// endif; ?>
-
 	</main>
-
-</div>
-<div class="reveal" id="exampleModal1" data-reveal>
-  <h1>Awesome. I Have It.</h1>
-  <p class="lead">Your couch. It is mine.</p>
-  <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
-  <button class="close-button" data-close aria-label="Close modal" type="button">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-
-
 
 <?php
 get_footer();
